c7dd289f03e35b662052142510aac99868602e1f
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2018-09-04 17:04:18 -0300

    First Commit

---
+++ src/main/java/com/msvvdv/bomovies/BoMoviesApplication.java
@@ -0,0 +1,12 @@
+package com.msvvdv.bomovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoMoviesApplication.class, args);
+    }
+}
